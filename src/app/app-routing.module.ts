335ae1a720da79bc37a5b3a853d7005e4bc8d245
Alejandro Fuentes <alejandrofuentes@Mac-mini-de-Alejandro.local>
2022-06-27 12:26:41 +0200

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./modules/home/home.module').then((m) => m.HomeModule),
-  },
-  {
-    path: 'contacts',
-    loadChildren: () => import('./modules/contacts/contacts.module').then((m) => m.ContactsModule),
-  },
-  {
-    path: 'profile/:name',
-    loadChildren: () => import('./modules/profile/profile.module').then((m) => m.ProfileModule),
-  },
-  {
-    path:'',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Tema4-Ejercicios'`, () => {
+  it(`should have as title 'angular-avanzado'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Tema4-Ejercicios');
+    expect(app.title).toEqual('angular-avanzado');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Tema4-Ejercicios app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-avanzado app is running!');
   });
 });
